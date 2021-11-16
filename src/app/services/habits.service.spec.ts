d025229f5416931117129855142431b4d21331a1
Jerax <dgjackon01@gmail.com>
2021-11-16 15:54:24 -0800

    add tests for all http requests in habits.service

--- src/app/services/habits.service.spec.ts
+++ src/app/services/habits.service.spec.ts
@@ -75,6 +75,8 @@ describe('HabitsService', () => {
 
       const req = httpTestingController.expectOne(service.habitsUrl)
 
+      expect(req.request.method).toEqual('GET');
+
       req.flush(emsg, { status: 404, statusText: 'Not Found' })
     })
   })
@@ -103,6 +105,57 @@ describe('HabitsService', () => {
       const req = httpTestingController.expectOne(`${service.habitsUrl}/${42}`);
 
       req.flush(msg, { status: 404, statusText: 'Not Found' });
+    })
+  })
+
+  describe('#updateHabit', () => {
+    it('should update matching habit', () => {
+      const putHabit = getTestHabits()[0];
+      const msg = "Habit Updated"
+
+      service.updateHabit(putHabit).subscribe(
+        reply => expect(reply).toEqual(msg)
+      )
+
+      const req = httpTestingController.expectOne(service.habitsUrl);
+
+      expect(req.request.method).toEqual('PUT');
+
+      req.flush(msg, { status: 200, statusText: 'OK' })
+    })
+  })
+
+  describe('#addHabit', () => {
+    it('should create habit', () => {
+      const addHabit = getTestHabits()[0];
+      const msg = "Habit Added"
+
+      service.addHabit(addHabit).subscribe(
+        reply => expect(reply).toEqual(addHabit)
+      )
+
+      const req = httpTestingController.expectOne(service.habitsUrl);
+
+      expect(req.request.method).toEqual('POST');
+
+      req.flush(addHabit, { status: 201, statusText: 'CREATED' })
+    })
+  })
+
+  describe('#deleteHabit', () => {
+    it('should delete matching habit', () => {
+      const deletedHabit = getTestHabits()[0];
+      const msg = "Habit Deleted"
+
+      service.deleteHabit(1).subscribe(
+        reply => expect(reply).toEqual(deletedHabit)
+      )
+
+      const req = httpTestingController.expectOne(`${service.habitsUrl}/1`);
+
+      expect(req.request.method).toEqual('DELETE');
+
+      req.flush(deletedHabit, { status: 202, statusText: 'ACCEPTED' })
     })
   })
 
